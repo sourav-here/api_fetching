8d794f717d713d28cde447c6b6805e9f9fd106fb
Sourav Raj <souravrajsrk11@gmail.com>
2024-04-25 09:39:35 +0530

    update

--- lib/services/fetch_service.dart
+++ lib/services/fetch_service.dart
@@ -5,8 +5,7 @@ import 'package:http/http.dart' as http;
 class Services {
   Future<List<ApiFetchModel>> getData() async {
     
-    final response = 
-          await http.get(Uri.parse('https:fakestoreapi.com/products'));
+    final response = await http.get(Uri.parse('https:fakestoreapi.com/products'));
     if (response.statusCode == 200) {
       final List<dynamic> jsonData = jsonDecode(response.body);
       return jsonData.map((json) => ApiFetchModel.fromJson(json)).toList();
